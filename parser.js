6716aef059e8958840b4ea6a9f6127b056af12c2
Jose Daniel Lopez <josedaniel2499@gmail.com>
2023-03-29 23:28:06 -0600

    choice completion and template fixes

--- parser.js
+++ parser.js
@@ -5,7 +5,7 @@ import { parse } from 'node:path';
 
 var template = fs.readFileSync('templates/template.ejs', 'utf-8');
 var choice = fs.readFileSync('templates/choice.ejs', 'utf-8');
-
+let DAMLFileText = `module Main where\n\nimport DA.List\nimport DA.Optional\nimport Daml.Script\n\n`
 
 ///////////////////////////////////// Create the JSON? ///////////////////////////////////////
 
@@ -31,14 +31,80 @@ let templates = [];
 for (let i = 0; i<Object.keys(json['templates']).length; i++)
 {
     let x = pad(i+1, 3);
-    templates.push(ejs.render(template, { object: json['templates']['Template'+x] }) + "\n\n");
+    templates.push(ejs.render(template, { object: json['templates']['template'+x] }) +"\n");
 }
 
 for (let i = 0; i<Object.keys(json['choices']).length; i++)
 {
     let x = pad(i+1, 3);
-    let owner = json['choices']['choice'+x]["Dependency"].split("T")[1]
-    templates[parseInt(owner)-1] += ejs.render(choice, { object: json['choices']['choice'+x] }) + "\n\n";
+
+    let owner = json['choices']['choice'+x]["dependency"].split("T")[1];
+    let next = json['choices']['choice'+x]["toCreate"].split("T")[1];
+
+    let origParams = json['templates']['template'+owner]["parameters"];
+    let withs = [];
+    let paramsToUse = [];
+
+
+    if(next)
+    {
+        let nextParams = json['templates']['template'+next]["parameters"];
+
+        for (const [key, value] of Object.entries(nextParams)){
+            paramsToUse.push([key])
+            if(!origParams.hasOwnProperty(key)){
+                withs.push([key,value]);
+            }
+        }
+    }
+    if(withs.length == 0){console.log(withs)}
+    templates[parseInt(owner)-1] += ejs.render(choice, { object: json['choices']['choice'+x], extras: withs, params: paramsToUse}) + "\n\n";
 }
 
 console.log(templates);
+
+templates.forEach( x =>{
+    DAMLFileText += x
+});
+
+
+DAMLFileText += `setup = script do
+
+    alice <- allocatePartyWithHint "Alice" (PartyIdHint "alice")
+    hugo <- allocatePartyWithHint "Hugo" (PartyIdHint "hugo")
+    bob <- allocatePartyWithHint "Bob" (PartyIdHint "bob")
+    paco <- allocatePartyWithHint "Paco" (PartyIdHint "paco")
+    luis <- allocatePartyWithHint "Luis" (PartyIdHint "luis")
+    aliceId <- validateUserId "alice"
+    bobId <- validateUserId "bob"
+    hugoId <- validateUserId "hugo"
+    pacoId <- validateUserId "paco"
+    luisId <- validateUserId "luis"
+
+    createUser (User aliceId (Some alice)) [CanActAs alice] 
+    createUser (User bobId (Some bob)) [CanActAs bob]
+    createUser (User hugoId (Some hugo)) [CanActAs hugo]
+    createUser (User pacoId (Some paco)) [CanActAs paco]
+    createUser (User luisId (Some luis)) [CanActAs luis]
+
+    pure ()`
+
+
+fs.mkdir("daml_output/daml",{ recursive: true }, (err) => {
+            if (err){
+                throw err;
+            }
+            else{
+                fs.writeFile("daml_output/daml/Step.daml", DAMLFileText, (err2) => {if (err2) throw (err2)})
+                fs.writeFile("daml_output/daml.yaml",`sdk-version: 2.1.1
+        name: project
+        source: daml
+        init-script: Main:setup
+        version: 0.0.1
+        dependencies:
+        - daml-prim
+        - daml-stdlib
+        - daml-script`, (err2) => {if (err2) throw (err2)})
+            }
+        })
+//exec('sed "s/\r/\n/g" daml_output/daml/Step.daml | sed "s/\t/    /g" > daml_output/daml/Main.daml')
